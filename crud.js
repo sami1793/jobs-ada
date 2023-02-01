db89f9f04584867df9d83cc5401d11bcb8fa598f
sami1793 <sami_17_94@hotmail.com>
2023-01-31 22:26:07 -0300

    Se crea renderJobs() para renderizar los empleos

--- crud.js
+++ crud.js
@@ -6,11 +6,35 @@ const getJobs = () =>{
     .then((response)=> response.json())
     .then((data)=>{
         console.log(data);
-        console.log(data[0]);
+        renderJobs(data);
     })
 }
 
 //Renderizar los trabajos
 const renderJobs = (jobs) =>{
-
+    $('#jobs-container').innerHTML =''
+    for (const {name, description, location, category, seniority, id} of jobs) {
+        $('#jobs-container').innerHTML += 
+        `<div class="card column is-3 m-5">
+        <div class="card-content">
+          <div class="media">
+            <div class="media-content">
+              <p class="title is-4">${name}</p>
+            </div>
+          </div>
+      
+          <div class="content">
+            <p>
+                <span>${description}</span>
+            </p>
+            <div class="tags">
+                <span class="tag is-warning is-light">${location}</span>
+                <span class="tag is-warning is-light">${category}</span>
+                <span class="tag is-warning is-light">${seniority}</span>
+            </div> 
+            <button class="button is-info is-small">See details</button>     
+          </div>
+        </div>
+    </div>`
+    }
 }
