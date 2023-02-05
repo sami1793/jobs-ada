eea6f82a2e52bba14c998b64c588ebdb6855fc84
sami1793 <sami_17_94@hotmail.com>
2023-02-05 10:02:39 -0300

    Se crea un filtro para cada input del formulario y se ejecuta todos con search button

--- filters.js
+++ filters.js
@@ -1,22 +1,46 @@
 const getJobsFiltered = () => {
+    
+    fetch(`${BASE_URL}/jobs`)
+      .then((response) => response.json())
+      .then((jobs) => {
+        renderJobs(jobsFiltered(jobs));
+      });
+  };
+
+
+//Filtrar por Location
+const filterLocation = (jobs, location) =>{
+    return jobs.filter((job) => job.location === location);
+}
+
+//Filtrar por Seniority
+const filterSeniority = (jobs, seniority) =>{
+    return jobs.filter((job) => job.seniority === seniority);
+}
+
+//Filtrar por Seniority
+const filterCategory = (jobs, category) =>{
+    return jobs.filter((job) => job.category === category);
+}
+
+//Ejecutar Filtros
+const jobsFiltered = (jobs) =>{
     let locationValue = $("#location-finder-input").value;
     let seniorityValue = $("#seniority-finder-input").value;
     let categoryValue = $("#category-finder-input").value;
-  fetch(`${BASE_URL}/jobs`)
-    .then((response) => response.json())
-    .then((jobs) => {
-      renderJobs(jobFiltered(jobs, locationValue, seniorityValue, categoryValue));
-    });
-};
 
-const jobFiltered = (jobs, location, seniority, category) => {
-    console.log(jobs)
-    console.log(location)
-  return jobs.filter(
-    (job) =>         
-        job.location === location// &&
-        // job.seniority === seniority &&
-        // job.category === category
-      
-  );
-};
+    if(locationValue!='Location'){
+        jobs = filterLocation (jobs, locationValue);
+    }
+
+    if(seniorityValue!='Seniority'){
+        jobs = filterSeniority (jobs, seniorityValue);
+    }
+
+    if(categoryValue!='Category'){
+        jobs = filterCategory (jobs, categoryValue);
+    }
+
+    return jobs
+} 
+
